849941e82019b5e5f4ea7e86946103f848cf3e40
Sam Hall <sam.hall@cdu.edu.au>
2015-07-18 01:07:34 +0930

    Windows keypresses are now my own. Lesson learnt: PyHook can really complicate your project. If you are a control freak like me, go straight to the Windows API via ctypes and researching on MSDN.
    I also considered AutoIT and AutoHotKey COM interfaces for this module, but in the end it seems Windows delivered everything I needed pretty easily.
    
    P.S. Python is in fact very easy to learn, seems reliable enough and has excellent people out there in the community publishing awesome stuff for you to quickly learn from.

--- hidemu/main.py
+++ hidemu/main.py
@@ -12,14 +12,14 @@ import hidemu.output.keystroker
 def main():
     """Launch app"""
     # TODO: Add a systray GUI component and reader polling loop (or work out how to listen for a new card)
-
     # TODO: Ensure only one instance is running at a time.
-
     reader = hidemu.reader.autodetect.Reader()
     key_stroker = hidemu.output.keystroker.KeyStroker()
     sn = reader.get_serial_number()
     if sn:
-        key_stroker.send_string('# %4MF^' + sn + ';\n')
+        # TODO: Detect and correctly report the Mifare UID byte size
+        # TODO: Confirm \r\n is cross platform enough, and if not do something platform detecty
+        key_stroker.send_string('#4MF^' + sn + ';\r\n')
     print(sn)
 
 if __name__ == '__main__':
--- hidemu/output/winkeystroker.py
+++ hidemu/output/winkeystroker.py
@@ -2,29 +2,72 @@
 # Copyright (c) 2015 Sam Hall, Charles Darwin University
 # See LICENSE.txt for details.
 #
-# winkeystroker.py - based on Plover modules
+# winkeystroker.py
 
 """KeyStroker class for Windows
 
-Essentially a simplified module based on plover/oslayer/winkeyboardcontrol for the sole purpose of emulating keystrokes.
+Windows module for emulating keystrokes required to output specified characters. This is about as simple as it gets.
+Probably could do with some error handling though as long as you stick to using the public methods on Windows platform
+it should be pretty hard to break. That said, it probably doesn't support unicode characters since it's using ANSI
+specific WinAPI functions.
 
 """
 
-import winkeyboardcontrol  # Plover code
+from ctypes import windll
+
+# VkKeyScanA high order flag key codes (SHIFT, CTRL, ALT)
+MODIFIER_KEYCODE = {1: 16,
+                    2: 17,
+                    4: 18}
 
 
 class KeyStroker:
-    """Emulate key strokes"""
+    """Emulate key strokes required to output specified characters"""
 
     def __init__(self):
-        # TODO: replace Plover modules with internal methods...
-        self.plover_kb_emu = winkeyboardcontrol.KeyboardEmulation()
+        pass
 
     def send_string(self, string):
         """Emulate typing a string"""
-        self.plover_kb_emu.send_string(string)
+        for character in string:
+            self.send_character(character)
 
-    def send_key(self, character, sync=1):
-        """Emulate typing a character (optionally syncing the display)"""
-        # TODO: in this case, this seems a bit silly, do I really need send_key?
-        self.send_string(character)
+    def send_character(self, character, sync=1):
+        """Emulate typing a character (sync ignored on this platform)"""
+        vk_code, modifiers = self._vk_and_modifiers_from_char(character[0])
+        self._apply_modifiers(modifiers)
+        self._key_down(vk_code)
+        self._key_up(vk_code)
+        self._release_modifiers(modifiers)
+
+    def _apply_modifiers(self, modifiers):
+        for bit in self._modifier_bit_split(modifiers):
+          self._key_down(MODIFIER_KEYCODE[bit])
+
+    def _release_modifiers(self, modifiers):
+        for bit in self._modifier_bit_split(modifiers):
+            self._key_up(MODIFIER_KEYCODE[bit])
+
+    @staticmethod
+    def _modifier_bit_split(modifiers):
+        while modifiers:
+            bit = modifiers & (~modifiers+1)
+            yield bit
+            modifiers ^= bit
+
+    @staticmethod
+    def _key_down(bvk):
+        bscan = windll.user32.MapVirtualKeyA(bvk, 0)
+        windll.user32.keybd_event(bvk, bscan, 0, 0)
+
+    @staticmethod
+    def _key_up(bvk):
+        bscan = windll.user32.MapVirtualKeyA(bvk, 0)
+        windll.user32.keybd_event(bvk, bscan, 0x0002, 0)
+
+    @staticmethod
+    def _vk_and_modifiers_from_char(key_char):
+        word_up = windll.user32.VkKeyScanA(ord(key_char))
+        vk = word_up & 0xff
+        modifiers = (word_up & 0xff00) >> 8
+        return vk, modifiers
